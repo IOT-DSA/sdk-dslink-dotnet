7c746e77bd3bbc27cd84e9902e1c53d4077ad12d
Logan Gorence <loganjohngorence@gmail.com>
2015-12-29 13:00:42 -0800

    Add more Node unit tests

--- UnitTests/src/Tests/NodeTests.cs
+++ UnitTests/src/Tests/NodeTests.cs
@@ -1,8 +1,8 @@
-﻿using NUnit.Framework;
-using DSLink.Nodes;
+﻿using DSLink.Nodes;
+using NUnit.Framework;
 using UnitTests.Framework;
 
-namespace UnitTests
+namespace UnitTests.Tests
 {
     [TestFixture]
     public class NodeTests
@@ -10,20 +10,44 @@ namespace UnitTests
         [Test]
         public void PathTest1()
         {
-            TestingContainer tc = new TestingContainer("PathTest1", true);
-            Node node = new Node("", null, tc);
+            var tc = new TestingContainer("PathTest1", true);
+            var node = new Node("", null, tc);
             Assert.AreEqual(node.Path, "/");
         }
 
         [Test]
         public void PathTest2()
         {
-            TestingContainer tc = new TestingContainer("PathTest2", true);
-            Node node = new Node("", null, tc);
-            Node child1 = node.CreateChild("Child1").BuildNode();
-            Node child2 = node.CreateChild("Child2").BuildNode();
+            var tc = new TestingContainer("PathTest2", true);
+            var child1 = tc.Responder.SuperRoot.CreateChild("Child1").BuildNode();
+            var child2 = tc.Responder.SuperRoot.CreateChild("Child2").BuildNode();
             Assert.AreEqual(child1.Path, "/Child1");
             Assert.AreEqual(child2.Path, "/Child2");
+        }
+
+        [Test]
+        public void SerializeTest1()
+        {
+            var tc = new TestingContainer("SerializeTest1", true);
+            var serialized = tc.Responder.SuperRoot.Serialize();
+            Assert.AreEqual(serialized[0][0], "$is");
+            Assert.AreEqual(serialized[0][1], "node");
+        }
+
+        [Test]
+        public void AbuseStructure1()
+        {
+            var tc = new TestingContainer("AbuseStructure1", true);
+            for (var i = 1; i <= 999; i++)
+            {
+                var node = tc.Responder.SuperRoot.CreateChild($"Test{i}").BuildNode();
+                for (var j = 1; j <= 999; j++)
+                {
+                    node.CreateChild($"Test{j}").BuildNode();
+                }
+                Assert.AreEqual(node.Children.Count, 999);
+            }
+            Assert.AreEqual(tc.Responder.SuperRoot.Children.Count, 999);
         }
     }
 }
