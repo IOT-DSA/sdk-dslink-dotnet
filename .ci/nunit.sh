41a3adff759e9d13b6099aa12bf73a897d6218bf
Logan Gorence <loganjohngorence@gmail.com>
2015-12-30 12:14:28 -0800

    Return exit code from nunit-console.

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 nuget install NUnit.Console -Version 3.0.1 -OutputDirectory packages
-mono packages/NUnit.Console.3.0.1/tools/nunit3-console.exe UnitTests/bin/Release/UnitTests.dll
+exit $(mono packages/NUnit.Console.3.0.1/tools/nunit3-console.exe UnitTests/bin/Release/UnitTests.dll)
