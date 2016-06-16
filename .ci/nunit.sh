9d94f3fcf124499931a1308bcee2eafa480e3e7a
Logan Gorence <loganjohngorence@gmail.com>
2016-06-16 13:03:08 -0700

    Change solution name for Travis CI and update NUnit Console runner.

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-nuget install NUnit.Console -Version 3.0.1 -OutputDirectory packages
-mono packages/NUnit.Console.3.0.1/tools/nunit3-console.exe UnitTests/bin/Release/UnitTests.dll
+nuget install NUnit.Console -Version 3.2.1 -OutputDirectory packages
+mono packages/NUnit.Console.3.2.1/tools/nunit3-console.exe UnitTests/bin/Release/UnitTests.dll
 exit $?
