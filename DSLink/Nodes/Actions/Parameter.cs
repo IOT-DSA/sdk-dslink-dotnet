e7590b3511563d3d9b50757546fe6650e2e06d73
Logan Gorence <Logan.Gorence@AcuityBrands.com>
2017-06-19 15:47:45 -0700

    Refactor Parameter to allow for object initializer and editor config.

--- DSLink/Nodes/Actions/Parameter.cs
+++ DSLink/Nodes/Actions/Parameter.cs
@@ -16,6 +16,10 @@ namespace DSLink.Nodes.Actions
             {
                 return this["name"].Value<string>();
             }
+            set
+            {
+                this["name"] = value;
+            }
         }
 
         public string ValueType
@@ -24,28 +28,56 @@ namespace DSLink.Nodes.Actions
             {
                 return this["type"].Value<string>();
             }
+            set
+            {
+                this["type"] = value;
+            }
         }
 
-        public string Default
+        public string DefaultValue
         {
             get
             {
                 return this["default"].Value<string>();
             }
+            set
+            {
+                this["default"] = value;
+            }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the
-        /// <see cref="T:DSLink.Nodes.Actions.Parameter"/> class.
-        /// </summary>
-        /// <param name="name">Name</param>
-        /// <param name="type">Type</param>
-        /// <param name="def">Default</param>
-        public Parameter(string name, string type, dynamic def = null)
+        public string Editor
         {
-            this["name"] = name;
-            this["type"] = type;
-            this["default"] = def;
+            get
+            {
+                return this["editor"].Value<string>();
+            }
+            set
+            {
+                this["editor"] = value;
+            }
+        }
+        
+        /// <summary>
+        /// Empty constructor to allow for object initializers.
+        /// </summary>
+        public Parameter()
+        {}
+
+        /// <summary>
+        /// Constructor that takes commonly used 
+        /// </summary>
+        /// <param name="name">Parameter name</param>
+        /// <param name="type">Parameter type</param>
+        /// <param name="def">Default value</param>
+        public Parameter(string name, string type, dynamic defaultValue = null, string editor = null)
+        {
+            Name = name;
+            ValueType = type;
+            if (defaultValue != null)
+                DefaultValue = defaultValue;
+            if (!string.IsNullOrEmpty(editor))
+                Editor = editor;
         }
     }
 }
