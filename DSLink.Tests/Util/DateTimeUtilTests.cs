fa8e73f229198b142e20ab72553af80dca70219d
Logan Gorence <loganjohngorence@gmail.com>
2018-01-04 17:29:47 -0800

    Take positive offset timezones into account for DateTimeUtil tests.

--- DSLink.Tests/Util/DateTimeUtilTests.cs
+++ DSLink.Tests/Util/DateTimeUtilTests.cs
@@ -2,6 +2,7 @@
 using DSLink.Util;
 using System;
 using FluentAssertions;
+using System.Text;
 
 namespace DSLink.Tests.Util
 {
@@ -12,7 +13,17 @@ namespace DSLink.Tests.Util
         public void DateTimeToISO8601Format()
         {
             DateTime dateTime = new DateTime(2000, 1, 1);
-            dateTime.ToIso8601().Should().StartWith("2000-01-01T00:00:00.000-");
+            var tzi = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+            var hours = tzi.Hours;
+            var mins = tzi.Minutes;
+            var expectedDate = new StringBuilder("2000-01-01T00:00:00.000");
+            expectedDate.Append(hours >= 0 ? '+' : '-');
+            if (hours < 0) hours *= -1;
+            expectedDate.Append(hours.ToString("00"));
+            expectedDate.Append(':');
+            expectedDate.Append(mins.ToString("00"));
+
+            dateTime.ToIso8601().Should().Equals(expectedDate.ToString());
         }
     }
 }
