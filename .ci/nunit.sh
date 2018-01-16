f9c62e0f52c56d3dbd1722b56f2140b828eac78d
Logan Gorence <loganjohngorence@gmail.com>
2018-01-15 21:48:35 -0800

    Remove unused .ci directory.

--- .ci/nunit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-nuget install NUnit.Console -Version 3.2.1 -OutputDirectory packages
-mono packages/NUnit.Console.3.2.1/tools/nunit3-console.exe UnitTests/bin/Release/UnitTests.dll
-exit $?
