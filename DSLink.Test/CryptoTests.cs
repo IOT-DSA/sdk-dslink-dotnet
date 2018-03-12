0409ec551a579d55ba854b59823ae36b31efd7fd
Logan Gorence <loganjohngorence@gmail.com>
2018-03-12 12:48:48 -0700

    Add new crypto tests.

--- DSLink.Test/CryptoTests.cs
+++ DSLink.Test/CryptoTests.cs
@@ -1,14 +1,17 @@
-﻿using DSLink.Connection;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using StandardStorage;
 using System;
 using System.Threading.Tasks;
+using DSLink.Connection;
 
 namespace DSLink.Test
 {
     [TestFixture]
     public class CryptoTests
     {
+        private const string KnownSavedKeyPair =
+            "AIjwCSLDZGF5G7BoncWBsBzIrAGhNNC+OJMbVm4Xirau " +
+            "BGSjQgTqbifSDbVIgOyh8T6cI+GJ0WwCksKK+l3sWF0THm5QpeCfzmj8iYmdgC185c30zAdHg8DB5+Pf/fU6QV4=";
         private IFolder _tempFolder;
 
         [SetUp]
@@ -25,6 +28,37 @@ namespace DSLink.Test
             await _tempFolder.DeleteAsync();
         }
 
-        // TODO: Write new crypto tests.
+        [Test]
+        public void Generate()
+        {
+            var keyPair = new KeyPair();
+            keyPair.Generate();
+            
+            Assert.NotNull(keyPair.EncodedPublicKey);
+            Assert.NotNull(keyPair.Save());
+        }
+
+        [Test]
+        public void GenerateSaveAndLoad()
+        {
+            var keyPair = new KeyPair();
+            keyPair.Generate();
+
+            var savedKeyPair = keyPair.Save();
+            var secondKeyPair = new KeyPair();
+            secondKeyPair.LoadFrom(savedKeyPair);
+
+            Assert.AreEqual(keyPair.EncodedPublicKey, secondKeyPair.EncodedPublicKey);
+            Assert.AreEqual(keyPair.Save(), secondKeyPair.Save());
+        }
+
+        [Test]
+        public void LoadKnownKeyPair()
+        {
+            var keyPair = new KeyPair();
+            keyPair.LoadFrom(KnownSavedKeyPair);
+            
+            Assert.AreEqual(KnownSavedKeyPair, keyPair.Save());
+        }
     }
 }
